845b2f8e054733573e9964b598954fa20500be64
Ruma303 <matteoh8@hotmail.it>
2025-07-03 10:17:24 +0200

    ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,22 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected title = 'angular-minicorso';
+  protected title = 'Angular Minicorso';
+  protected author = 'Riccardo Degni';
+
+  x = 10;
+
+  students = [
+    { id: 1, name: 'Mario Rossi', age: 20, city: 'Roma' },
+    { id: 2, name: 'Luca Bianchi', age: 22, city: 'Milano' },
+    { id: 3, name: 'Anna Verdi', age: 19, city: 'Napoli' },
+    { id: 4, name: 'Giulia Neri', age: 21, city: 'Torino' },
+    { id: 5, name: 'Marco Gallo', age: 23, city: 'Firenze' }
+  ];
 }
