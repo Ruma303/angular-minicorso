2e9bdde0532c2bfa263b9af7cbb28768cadb2b96
Ruma303 <matteoh8@hotmail.it>
2025-07-05 10:52:58 +0200

    pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
 import { Component, signal, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { Card } from './components/card/card';
+import { CapitalizePipe } from './utils/capitalize-pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, Card],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, FormsModule, CapitalizePipe],
+  template: `
+  <section>
+    <div>
+      {{ text | capitalize }}
+    </div>
+  </section>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.css']
 })
 export class App {
+  text = "lorem ipsum dolor sit amet consectetur adipisicing elit.";
 }
