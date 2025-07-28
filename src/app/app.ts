3309ad82cce14107d8cb6a5dd7f887280342e3cb
Ruma303 <matteoh8@hotmail.it>
2025-07-28 17:31:34 +0200

    aggiunti linked signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal, output } from '@angular/core';
+import { Component, signal, output, linkedSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Card } from './components/card/card';
@@ -14,10 +14,38 @@ import { NotifyButton } from "./components/notify-button/notify-button";
 })
 export class App {
   number = signal(0);
+  m = signal<number>(0);
+  n = signal<number>(0);
 
   messageReceived = signal<string>('');
 
   onMessageReceived(message: string) {
     this.messageReceived.set(message);
   }
+
+  linkedAsComputed = linkedSignal(() => this.number() * 3); // Uso come un computed signal
+
+  linkedSignal = linkedSignal({
+    source: this.number,
+    computation: (source, prev) => {
+      console.log('number', this.number());
+      console.log('prev', prev?.source);
+      console.log('number * 2', prev?.source !== undefined ? prev.source * 2 : undefined);
+
+      return source as number * 3;
+    }
+  });
+
+  linkedWithMultipleSources = linkedSignal({
+    source: () => ({
+      m: this.m(),
+      n: this.n()
+    }),
+    computation: ({ m, n }, prev) => {
+      console.log('source1', m);
+      console.log('source2', n);
+      console.log('prev', prev?.source);
+      return m + n * 2;
+    }
+  });
 }
